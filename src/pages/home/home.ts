ee619313920ea1abe226a5fc550c05696e4bfed7
Alberto Valverde <avalverde@serbatic.es>
2017-10-27 09:41:46 +0200

    Load users with provider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpProvider } from '../../providers/http/http';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  	selector: 'page-home',
+  	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	users : any[];
 
-  }
+  	constructor(public navCtrl: NavController, public http: HttpProvider) {
+
+  	}
+
+  	loadUsers(){
+    	this.http.loadUsers().subscribe((res) => {
+          	this.users = res.results;
+          	console.log(this.users)
+        }, (err) =>{
+          	console.log(err);
+        });
+  	}
 
 }
